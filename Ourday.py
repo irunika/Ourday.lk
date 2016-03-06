2ce998a04ec16533e203555acf217100b2a246fd
tharinda <tharinda221@gmail.com>
2016-03-06 11:14:00 +0530

    Created facebook authentication

--- Ourday.py
+++ Ourday.py
@@ -2,11 +2,12 @@ __author__ = 'Irunika'
 
 from flask_restful import Api
 import os
-from flask import Flask, render_template
+from flask import Flask
 
 from restful_services.Index import *
 from restful_services.AddUser import *
 from restful_services.SendCities import *
+from restful_services.Authentication import *
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,6 +18,8 @@ api.add_resource(Index, '/', endpoint='/')
 api.add_resource(Ourday, '/ourday', endpoint='/ourday')
 api.add_resource(AddUser, '/register_user', endpoint='/register_user')
 api.add_resource(SendCities, '/get_cities', endpoint='/get_cities')
+api.add_resource(facebookLogin, '/authorize/facebook', endpoint='/authorize/facebook')
+api.add_resource(facebookCallBack, '/callback/facebook', endpoint='/callback/facebook')
 
 if __name__ == '__main__':
     app.run(debug=True)
